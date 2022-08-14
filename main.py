0e4c8a36930c28a8c6d96d7e43e0aa0a260b1198
harsha-mangena <44197358+harsha-mangena@users.noreply.github.com>
2022-08-14 19:49:58 +0000

    Added Comments

--- main.py
+++ main.py
@@ -19,22 +19,28 @@ def main(command_params):
         command, arguments = get_commands_and_arguments_from_file(line)
 
         if command == 'Create_parking_lot':
+          assert len(arguments) == 1, "Create_parking_lot needs number of slots!"
           parking_lot.create_parking_lot(int(arguments[0]))
 
         elif command == 'Park':
+          assert len(arguments) == 3, "Park command requires Registration number and driver age"
           car = Car(arguments[0], arguments[2])
           parking_lot.park(car)
 
         elif command == 'Leave':
+          assert len(arguments) == 1, "Leave command requires slot number needed to be freed"
           parking_lot.leave(int(arguments[0]))
 
         elif command == 'Vehicle_registration_number_for_driver_of_age':
+          assert len(arguments) == 1, "Vehicle_registration_number_for_driver_of_age requires driver age"
           parking_lot.registration_numbers_for_cars_for_driver_age(arguments[0])
 
         elif command == 'Slot_numbers_for_driver_of_age':
+          assert len(arguments) == 1, "Slot_numbers_for_driver_of_age requires driver age"
           parking_lot.slot_numbers_for_cars_for_driver_age(arguments[0])
 
         elif command == 'Slot_number_for_car_with_number':
+          assert len(arguments) == 1, "Slot_number_for_car_with_number requires registration number"
           parking_lot.slot_number_for_registration_number(arguments[0])
 
         else:
--- src/ParkingLot.py
+++ src/ParkingLot.py
@@ -22,16 +22,27 @@ class ParkingLot:
         self.age_registration_mapping = defaultdict(list)
         self.slot_car_mapping  = OrderedDict()
         self.available_parking_lots = []
-
+      
+    '''
+    Creating the new parking lot with given size.
+    And we will push the available slots(i.e all when the lot is created into min Heap)
+    '''
     def create_parking_lot(self, total_slots):
         print("Created a parking lot with {} slots".format(total_slots))
         for i in range(1, total_slots + 1):
             heapq.heappush(self.available_parking_lots, i)
         return True
 
+    '''
+    Check if the slot is available for parking, if not return none
+    '''
     def get_nearest_slot(self):
         return heapq.heappop(self.available_parking_lots) if self.available_parking_lots else None
 
+    '''
+    If the provided slot is already empty, do nothing.
+    else get the car from the slot and remove the car details and make the slot available 
+    '''
     def leave(self, slot_to_be_freed):
         found = None
         for registration_no, slot in self.registration_slot_mapping.items():
@@ -44,7 +55,7 @@ class ParkingLot:
             car_to_leave = self.slot_car_mapping[slot_to_be_freed]
             self.age_registration_mapping[car_to_leave.driver_age].remove(found)
             del self.slot_car_mapping[slot_to_be_freed]
-            print("Slot number {0} vacated, the car with vehicle registration number {1} left the space, the driver of the car was of age {2}"\
+            print("Slot number {0} vacated, the car with vehicle registration number {1} left the space, the driver of the car was of age {2}" 
                   .format(slot_to_be_freed, \
                           car_to_leave.registration_number, \
                           car_to_leave.driver_age))
@@ -55,6 +66,10 @@ class ParkingLot:
             print("slot is not in use")
             return False
 
+    '''
+    If all slots are full we can't park, return false
+    else get the available slot by using get_nearest_slot(), and store the details in hashMaps
+    '''
     def park(self, car):
         slot_no = self.get_nearest_slot()
         if slot_no is None:
@@ -67,13 +82,15 @@ class ParkingLot:
         self.age_registration_mapping[car.driver_age].append(car.registration_number)
         return slot_no
 
-    # Registration numbers of all cars of a specific age
+    '''
+    As we are using the hashMaps to store the slot allocated with respective to driver age, return that key(driver 
+    age)
+    '''
     def registration_numbers_for_cars_for_driver_age(self, driver_age):
         registration_numbers = self.age_registration_mapping[driver_age]
         print(", ".join(registration_numbers))
         return self.age_registration_mapping[driver_age]
 
-    # Slot numbers of all slots where a car of specific aged driver is parked
     def slot_numbers_for_cars_for_driver_age(self, driver_age):
         registration_numbers = self.age_registration_mapping[driver_age]
         slots = [self.registration_slot_mapping[reg_no] for reg_no in registration_numbers]
